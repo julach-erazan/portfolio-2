87e104e054b6ff2986574c311aa872a21f5d38aa
julach-earzan <jesrilanka@gmail.com>
2026-02-26 00:31:28 +0530

    Create allJobs.js

---
+++ .content-collections/generated/allJobs.js
@@ -0,0 +1,51 @@
+
+export default [
+  {
+    "jobTitle": "Intern Software Engineer",
+    "summary": "Contributed to hospital management solutions and built responsive UIs using React and TypeScript.",
+    "startDate": "2024-03",
+    "endDate": "2024-09",
+    "company": "Invicta Innovations (Pvt) Ltd",
+    "location": "Sri Lanka",
+    "tags": [
+      "React",
+      "TypeScript",
+      "Node.js",
+      "Hospital Management",
+      "UI/UX"
+    ],
+    "content": "- Contributed to the Pediatric Management System, a hospital management solution.\r\n- Built responsive UIs using React and TypeScript with backend API integration.\r\n- Collaborated with designers and developers to meet project goals.\r\n- Focused on debugging and testing to ensure application stability.\r\n- Gained experience in real-world development, teamwork, and project execution.",
+    "_meta": {
+      "filePath": "invicta-innovations.md",
+      "fileName": "invicta-innovations.md",
+      "directory": ".",
+      "extension": "md",
+      "path": "invicta-innovations"
+    }
+  },
+  {
+    "jobTitle": "Associate Software Engineer",
+    "summary": "Developing and maintaining Restaurant and Reservation Management Systems using Next.js and PostgreSQL.",
+    "startDate": "2025-02",
+    "company": "X4 Digital Labs",
+    "location": "Remote/Sri Lanka",
+    "tags": [
+      "Next.js",
+      "React",
+      "PostgreSQL",
+      "Redux",
+      "JWT",
+      "Adyen",
+      "HyperPay",
+      "Agile"
+    ],
+    "content": "- Developing and maintaining Restaurant Management System and Reservation Management System using Next.js and PostgreSQL.\r\n- Built responsive landing pages with optimized performance and user experience.\r\n- Implemented state management with Redux and Context API to ensure smooth data flow.\r\n- Integrated JWT authentication for secure access control.\r\n- Worked on payment gateway integrations (Adyen & HyperPay) enabling secure online payments.\r\n- Collaborated in Agile teams, participating in code reviews and sprint planning.\r\n- **Key Achievement:** Improved system performance by optimizing PostgreSQL queries, reducing API response time by 20%.",
+    "_meta": {
+      "filePath": "x4-digital-labs.md",
+      "fileName": "x4-digital-labs.md",
+      "directory": ".",
+      "extension": "md",
+      "path": "x4-digital-labs"
+    }
+  }
+]
