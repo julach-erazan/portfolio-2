c2c965d8591dd0f287276b5483599de9498e8e7c
julach-earzan <jesrilanka@gmail.com>
2026-02-26 00:31:24 +0530

    Create allEducations.js

---
+++ .content-collections/generated/allEducations.js
@@ -0,0 +1,22 @@
+
+export default [
+  {
+    "school": "Eastern University, Sri Lanka",
+    "summary": "Bachelor of Computer Science (Trincomalee Campus)",
+    "startDate": "2020",
+    "endDate": "2024",
+    "tags": [
+      "Computer Science",
+      "Software Engineering",
+      "Web Development"
+    ],
+    "content": "- Focused on high-quality software engineering principles and full-stack development.\r\n- Relevant Coursework: Data Structures, Algorithms, Database Management, Web Engineering.\r\n- GCE Advanced Level (Physical Science) - 2018\r\n- GCE Ordinary Level - 2015",
+    "_meta": {
+      "filePath": "eastern-university.md",
+      "fileName": "eastern-university.md",
+      "directory": ".",
+      "extension": "md",
+      "path": "eastern-university"
+    }
+  }
+]
