051e9d12b424187cc6618fd1d4f450309bd2c278
julach-earzan <jesrilanka@gmail.com>
2026-02-26 21:01:16 +0530

    update

---
+++ .output/server/chunks/build/_tanstack-start-manifest_v-mJp2EXIJ.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "D:/Santha Project/portfolio-2/src/routes/__root.tsx", "children": ["/", "/api/download-cv", "/api/resume-chat"], "preloads": ["/assets/main-DlLhyTgj.js"], "assets": [] }, "/": { "filePath": "D:/Santha Project/portfolio-2/src/routes/index.tsx", "assets": [{ "tag": "link", "attrs": { "rel": "stylesheet", "href": "/assets/index-0yo7F1DJ.css", "type": "text/css" } }], "preloads": ["/assets/index-gtc-kZQc.js"] }, "/api/download-cv": { "filePath": "D:/Santha Project/portfolio-2/src/routes/api.download-cv.ts" }, "/api/resume-chat": { "filePath": "D:/Santha Project/portfolio-2/src/routes/api.resume-chat.ts" } }, "clientEntry": "/assets/main-DlLhyTgj.js" });
+export {
+  tsrStartManifest
+};
